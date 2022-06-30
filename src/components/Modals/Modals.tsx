9db01e2b29c9f4f010e2e766b26d40134af9bede
codethi <docencia.thiago@gmail.com>
2022-06-29 22:56:19 -0300

    refactor: remove /

--- src/components/Modals/Modals.tsx
+++ src/components/Modals/Modals.tsx
@@ -40,7 +40,7 @@ function Modals({
     identity: "",
   });
 
-  const baseURL = "https://api-marvelcharacters.herokuapp.com//characters";
+  const baseURL = "https://api-marvelcharacters.herokuapp.com/characters";
 
   const getCharacterById = async () => {
     const resp = await fetch(`${baseURL}/${id}`, {
